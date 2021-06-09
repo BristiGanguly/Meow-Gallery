e82fbb58f5c7c1e91f267a7cc4d1ef710587cfc4
BristiGanguly <gangulybristi@gmail.com>
2021-06-09 18:41:41 +0530

    fetch1

---
+++ Meow-Gallery/src/main/java/photoalbum/AlbumDTO.java
@@ -0,0 +1,42 @@
+package photoalbum;
+
+public class AlbumDTO {
+
+	private String path;
+	private String name;
+	private String tags;
+	private String desc;
+
+	public String getPath() {
+		return path;
+	}
+
+	public void setPath(String path) {
+		this.path = path;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getTags() {
+		return tags;
+	}
+
+	public void setTags(String tags) {
+		this.tags = tags;
+	}
+
+	public String getDesc() {
+		return desc;
+	}
+
+	public void setDesc(String desc) {
+		this.desc = desc;
+	}
+
+}
